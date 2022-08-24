194c4744cc5fda1acdb68b656a45881d6c8df172
Mario Alvarez <mario.alvarez1810@gmail.com>
2022-08-24 15:07:38 -0500

    Se crea proyecto básico de Spring

---
+++ learning-spring-with-springboot/src/main/java/com/generation/learningspringwithspringboot/LearningSpringWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.generation.learningspringwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ learning-spring-with-springboot/src/test/java/com/generation/learningspringwithspringboot/LearningSpringWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.learningspringwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
